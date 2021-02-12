4c3d339ee38f302edc57638c77d859ff830955a7
nishanth <gayatridipali@gmail.com>
2021-02-12 19:15:48 +0530

    added title of product pg as product name

--- pages/products/[productId].tsx
+++ pages/products/[productId].tsx
@@ -4,6 +4,7 @@ import styles from "../../styles/Home.module.css";
 import { withUrqlClient } from "next-urql";
 import { CreateUrqlClient } from "../../utils/createUrqlClient";
 import { useProductQuery } from "../../src/generated/graphql";
+import Head from "next/head";
 
 const ProductPage: NextPage<{ productId: string }> = ({ productId }) => {
   const [{ data, fetching, error }] = useProductQuery({
@@ -17,7 +18,14 @@ const ProductPage: NextPage<{ productId: string }> = ({ productId }) => {
   if (error) {
     return <p>{error.message}</p>;
   }
-  return <Layout className={styles.body}>{data?.product?.title || ""}</Layout>;
+  return (
+    <>
+      <Head>
+        <title>{data?.product?.title || "Loading..."}</title>
+      </Head>
+      <Layout className={styles.body}>{data?.product?.title || ""}</Layout>
+    </>
+  );
 };
 
 ProductPage.getInitialProps = ({ query }) => {
